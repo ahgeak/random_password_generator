20561cc40e4f5c1b326bf76ca46e7ce9c4bb369a
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-10-03 16:06:04 -0700

    added psudo-code to javascript file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
-// Assignment code here
+// User clicks the Generate Password button (id="generate" class="btn")
+
+// User is prompted to specify length of password (range 8 - 128 characters). This input must be validaded to ensure it is correct range. Assign value to passwordLength use if statement to say if (passwordLength > 8 || passwordLength < 128) --> tell user invalid input new number
+
+// User is presented with a list of password criteria that include: lowercase, uppercase, numeric, and/or special characters. Check box with options or maybe a list to choose from
+
+// Console log the chosen criteria. Ensure at least one criteria is selected. Provide an error if no options were chosen or if incorrect input
+
+// Once all prompts are provided the pasword is generated.
+
+// Generated password is displayed in an alert message
 
 
 // Get references to the #generate element
